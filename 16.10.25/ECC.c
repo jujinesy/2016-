7ab0f77cf18236175b3a9d03cfeba8565cc2d808
Ju Jin BAE <jujinesy@gmail.com>
2016-10-25 22:11:24 +0900

    update 16.10.25

--- 16.10.25/ECC.c
+++ 16.10.25/ECC.c
@@ -9,6 +9,15 @@ void mpz_add_mod(mpz_t r, const mpz_t a, const mpz_t b, const mpz_t n)
 	//mpz_mod(r, r, n);
 }
 // r = a-b mod n
-void mpz_sub_mod(mpz_t r, const mpz_t a, const mpz_t b, const mpz_t n);
+void mpz_sub_mod(mpz_t r, const mpz_t a, const mpz_t b, const mpz_t n)
+{
+	mpz_sub(r, a, b);
+	if (mpz_sgn(r) < 0)	mpz_add(r, r, n);
+	//mpz_mod(r, r, n);
+}
 // r = a*b mod n
-void mpz_mul_mod(mpz_t r, const mpz_t a, const mpz_t b, const mpz_t n);
+void mpz_mul_mod(mpz_t r, const mpz_t a, const mpz_t b, const mpz_t n)
+{
+	mpz_mul(r, a, b);
+	mpz_mod(r, r, n);
+}
